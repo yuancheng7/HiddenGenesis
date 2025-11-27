662136574e77c344f6debdfd28fd79708fc16e7f
yuancheng7 <yerldu77611@outlook.com>
2025-11-27 16:08:46 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedFactory = await deploy("ConfidentialTokenFactory", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ConfidentialTokenFactory contract: `, deployedFactory.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_confidentialTokenFactory"; // id required to prevent reexecution
+func.tags = ["ConfidentialTokenFactory"];
